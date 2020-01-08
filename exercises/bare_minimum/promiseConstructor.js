ab523b393529c80fbf9e7e4b7ef7c960e8ac3e15
JohnCharamonde <john.charamonde@gmail.com>
2020-01-08 09:17:29 -0500

    Completed basic requirements for promises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,14 +9,37 @@ var request = require('request');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
+var pluckFirstLineFromFileAsync = function (filePath) {
   // TODO
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, 'utf8', (err, file) => { // What is utf8?
+      if (err) {
+        reject(err);
+      } else {
+        resolve(file.split('\n')[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
+var getStatusCodeAsync = function (url) {
   // TODO
+  return new Promise(function(resolve, reject) {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
 };
+
+// This function violates rule (2) of the node style callback pattern,
+// therefore we have to reimplement it using the `new Promise` constructor
+
+
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
